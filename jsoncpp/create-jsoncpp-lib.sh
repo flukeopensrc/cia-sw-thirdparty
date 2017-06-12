4e58522e1a24e5156735a7872ac4b5bac62af229
Trevor Vannoy <tvannoy@flukecal.com>
2017-06-12 16:05:48 -0700

    Updated to GCC 5 / Quartus 17.0

--- jsoncpp/create-jsoncpp-lib.sh
+++ jsoncpp/create-jsoncpp-lib.sh
@@ -26,7 +26,11 @@ INCDIR=.
 
 #* Misc config ***************************************************************
 #-D_GLIBCXX_USE_C99 ensures std::stoul and some others are defined.
-CXXFLAGS="-std=c++11 -D_GLIBCXX_USE_C99"
+#-D_GLIBCXX_USE_CXX11_ABI=0 forces gcc to use the old std::string ABI,
+# which isn't c++11 compliant... After upgrading to GCC 5, I couldn't get
+# everything to link properly without using the old ABI; I suspect there is an
+# IntelFPGA library somewhere, which I don't have control over, that uses the old ABI
+CXXFLAGS="-std=c++11 -D_GLIBCXX_USE_C99 -D_GLIBCXX_USE_CXX11_ABI=0"
 
 #* Build the app *************************************************************
 echo "Generating library makefiles..."
@@ -43,12 +47,13 @@ nios2-lib-generate-makefile \
 echo
 echo "Running make..."
 echo
-cd $BUILDDIR
-make
+cd $BUILDDIR &&
+make &&
 
 echo
 echo "Moving the library to the lib directory..."
 echo
-mv "lib$LIBNAME.a" "../lib/lib$LIBNAME.a"
+mkdir -p ../lib
+mv "lib$LIBNAME.a" "../lib/lib$LIBNAME.a" &&
 
 echo "All done!"
